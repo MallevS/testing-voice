5c88d0f845fe45f7aaa6473be95c24050ba614b9
MallevS <malevstojan@gmail.com>
2025-10-16 22:30:08 +0200

    testing public tcp

--- src/app/api/twilio/voice/route.ts
+++ src/app/api/twilio/voice/route.ts
@@ -1,15 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const twiml = `
-  <Response>
-  <Connect>
-    <Stream url="wss://testing-voice-amber.vercel.app/api/realtime-stream" />
-  </Connect>
-</Response>
-
+    const twiml = `
+    <Response>
+      <Connect>
+        <Stream url="wss://voice-realtime-bridge.fly.dev" />
+      </Connect>
+    </Response>
   `;
-
   return new NextResponse(twiml, {
     headers: { "Content-Type": "text/xml" },
   });
